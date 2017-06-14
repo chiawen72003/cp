6ba2ea03e968ad72b672eede184a2e17388bdf65
cwc <cwc>
2017-06-14 22:03:23 +0800

    增加問卷列表的首頁

---
+++ application/views/questionnaire/index.php
@@ -0,0 +1,33 @@
+<div id="wrapper">
+    <div class="search">
+        <!--模組名稱搜尋：<input type="text" name="s_word" id="s_word" value="" ><a class="button" title="模組名稱搜尋：" onclick="search_word()">搜尋</a><a href="index.php" class="button" title="清除搜尋" >清除搜尋</a>-->
+    </div>
+    <ul>
+        <li><a class="button" title="新增模組" onclick="add_Model()">新增問卷</a></li>
+    </ul>
+    <table class="title">
+        <tbody>
+        <tr>
+            <td width="5%">編號</td>
+            <td width="50%">問卷標題</td>
+            <td width="45%">功能設定</td>
+        </tr>
+        </tbody>
+    </table>
+    <!-- 單元 -->
+    <div class="accordionButton">
+        <table class="list_item">
+            <tbody>
+            <tr>
+                <td width="5%">1</td>
+                <td width="50%">問卷標題</td>
+                <td width="45%">
+                    <a class="button" title="編輯" href="add.html">編輯</a>
+                    <a class="button" title="刪除" onclick="">刪除</a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <!-- 單元 End -->
+</div>
