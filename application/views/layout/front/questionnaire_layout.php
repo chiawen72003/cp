827b74aa8080ac9d70e55c7b025e0b6ffe6c830d
cwc <cwc>
2017-06-17 17:43:55 +0800

    增加 填寫問卷的layout

---
+++ application/views/layout/front/questionnaire_layout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html class="questionnaire">
+<head>
+    <meta charset="UTF-8">
+    <title>Ntcu.</title>
+    <!-- Style Sheets -->
+    <link rel="stylesheet" href="<?php echo $base;?>public/css/questionnaire/style.css?v=0.12" />
+    <!--[if lt IE 9]>
+    <script src="<?php echo base_url('public/js/html5shiv.js'); ?>"> </script>
+    <![endif]-->
+</head>
+<body>
+<?php
+//此處插入資料
+echo $content_for_layout;//顯示view的區塊
+?>
+<script src="<?php echo base_url('public/js/questionnaire/jquery.js'); ?>"> </script>
+<script src="<?php echo base_url('public/js/icheck.js'); ?>"> </script>
+<script src="<?php echo base_url('public/js/main.js'); ?>"> </script>
+</body>
+</html>
