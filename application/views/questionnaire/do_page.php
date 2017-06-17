e55cc1b0ee69064fa0a57f7ece58b60c5c425ef9
cwc <cwc>
2017-06-17 17:44:10 +0800

    增加 填寫問卷的內頁

---
+++ application/views/questionnaire/do_page.php
@@ -0,0 +1,195 @@
+
+<div id="mainBody">
+    <div id="page-header">
+        <div class="page-title">問卷標題</div>
+    </div>
+    <div id="page-container">
+        <!-- page-body -->
+        <div id="page-body">
+            <div class="form-wrap">
+                <div class="add-wrap questionnaire-title">
+                    <div class="form-group">
+                        <div class="questionnaire-h1">問卷標題</div>
+                    </div>
+                    <div class="form-group">
+                        <div class="questionnaire-con">
+                            問卷說明
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="add-wrap">
+                    <!-- 分數 -->
+                    <div class="form-area">
+                        <div class="form-area-drag"><i class="material-icons">drag_handle</i></div>
+                        <div class="form-group">
+                            <div class="form-group form-fraction clearfix">
+                                <span>問答題</span>
+                                <span>( 每小題 </span>
+                                <span>20</span>
+                                <span> 分，共 </span>
+                                <span>40</span>
+                                <span> 分 )</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 分數 end -->
+                    <!-- 簡答 -->
+                    <div class="form-area">
+                        <div class="form-area-drag"><i class="material-icons">drag_handle</i></div>
+                        <div class="form-group form-group-title">
+                            <div class="title">
+                                未命名簡答問題
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-group-list">
+                                <!-- 編輯狀態 -->
+                                <div class="form-group">
+                                    <div class="form-group-list-li">
+                                        <input type="text" class="form-control" placeholder="簡答文字" />
+                                    </div>
+                                </div>
+                                <!-- 編輯狀態 end -->
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 簡答 end -->
+                    <!-- 段落 -->
+                    <div class="form-area">
+                        <div class="form-area-drag"><i class="material-icons">drag_handle</i></div>
+                        <div class="form-group form-group-title">
+                            <div class="title">
+                                未命名段落問題
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-group-list">
+                                <!-- 編輯狀態 -->
+                                <div class="form-group">
+                                    <div class="form-group-list-li">
+                                        <textarea rows='1' class="form-control" placeholder='詳答文字'></textarea>
+                                    </div>
+                                </div>
+                                <!-- 編輯狀態 end -->
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 段落 end -->
+                </div>
+                <div class="add-wrap">
+                    <!-- 分數 -->
+                    <div class="form-area">
+                        <div class="form-area-drag"><i class="material-icons">drag_handle</i></div>
+                        <div class="form-group">
+                            <div class="form-group form-fraction clearfix">
+                                <span>選擇題</span>
+                                <span>( 每小題 </span>
+                                <span>10</span>
+                                <span> 分，共 </span>
+                                <span>60</span>
+                                <span> 分 )</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 分數 end -->
+                    <!-- 單選 -->
+                    <div class="form-area">
+                        <div class="form-area-drag"><i class="material-icons">drag_handle</i></div>
+                        <div class="form-group form-group-title">
+                            <div class="title">
+                                未命名單選問題
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-group-list">
+                                <!-- 非編輯狀態 -->
+                                <div class="form-group-list-li">
+                                    <div class="form-group-list-li__click">
+                                        <input type="radio" class="form-radio" name="radio1">
+                                    </div>
+                                    <div class="form-group-list-li__input">
+                                        <div class="form-group-list-li_name">radio 1</div>
+                                    </div>
+                                </div>
+                                <div class="form-group-list-li">
+                                    <div class="form-group-list-li__click">
+                                        <input type="radio" class="form-radio" name="radio1">
+                                    </div>
+                                    <div class="form-group-list-li__input">
+                                        <div class="form-group-list-li_name">radio 2</div>
+                                    </div>
+                                </div>
+                                <div class="form-group-list-li">
+                                    <div class="form-group-list-li__click">
+                                        <input type="radio" class="form-radio" name="radio1">
+                                    </div>
+                                    <div class="form-group-list-li__input">
+                                        <div class="form-group-list-li_name">radio 3</div>
+                                    </div>
+                                </div>
+                                <!-- 非編輯狀態 end -->
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 單選 end -->
+                    <!-- 多選 -->
+                    <div class="form-area">
+                        <div class="form-area-drag"><i class="material-icons">drag_handle</i></div>
+                        <div class="form-group form-group-title">
+                            <div class="title">
+                                未命名多選問題
+                            </div>
+                        </div>
+                        <div class="form-gro form-titleup">
+                            <div class="form-group-list">
+                                <!-- 非編輯狀態 -->
+                                <div class="form-group-list-li">
+                                    <div class="form-group-list-li__click">
+                                        <input type="checkbox" class="form-checkbox" name="checkbox1">
+                                    </div>
+                                    <div class="form-group-list-li__input">
+                                        <div class="form-group-list-li_name">checkbox 1</div>
+                                    </div>
+                                </div>
+                                <div class="form-group-list-li">
+                                    <div class="form-group-list-li__click">
+                                        <input type="checkbox" class="form-checkbox" name="checkbox1">
+                                    </div>
+                                    <div class="form-group-list-li__input">
+                                        <div class="form-group-list-li_name">checkbox 2</div>
+                                    </div>
+                                </div>
+                                <div class="form-group-list-li">
+                                    <div class="form-group-list-li__click">
+                                        <input type="checkbox" class="form-checkbox" name="checkbox1">
+                                    </div>
+                                    <div class="form-group-list-li__input">
+                                        <div class="form-group-list-li_name">checkbox 3</div>
+                                    </div>
+                                </div>
+                                <!-- 非編輯狀態 end -->
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 多選 end -->
+                </div>
+                <div class="questionnaire-button">
+                    <button class="btn btn-submit">提交</button>
+                </div>
+            </div>
+        </div>
+        <!-- page-body end -->
+
+        <!-- gotop -->
+        <div class="page-gotop">
+            <a href="#" class="gotop"><i class="material-icons">vertical_align_top</i></a>
+        </div>
+        <!-- gotop end -->
+    </div>
+    <div id="page-footer">
+        <div class="copyright">
+            Copyright © National Taichung University of Education
+        </div>
+    </div>
+</div>
